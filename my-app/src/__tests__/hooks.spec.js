06756158c19c484dab514a95c9112639ca38e032
dianavolt <diana.voltchok@telus.com>
2019-03-19 20:42:57 -0800

    Add hook unit test with fakeTimers to useDebounce

--- my-app/src/__tests__/hooks.spec.js
+++ my-app/src/__tests__/hooks.spec.js
@@ -1,6 +1,11 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 
-import { useSearchForm } from '../hooks'
+import {
+    useSearchForm,
+    useDebounce
+} from '../hooks'
+
+jest.useFakeTimers()
 
 describe('useSearchForm hook', () => {
     let event1
@@ -32,5 +37,37 @@ describe('useSearchForm hook', () => {
         act(() => result.current.onSearchChange(event2))
 
         expect(result.current.searchVal).toBe('input2')
+    })
+})
+
+describe('useDebounce hook', () => {
+    it('it should return the same value after long delay', () => {
+        const expectedResults = 'text'
+        const { result } = renderHook(() => useDebounce(expectedResults, 500))
+
+        expect(result.current).toBe(expectedResults)
+
+        jest.advanceTimersByTime(520)
+
+        expect(result.current).toBe(expectedResults)
+    })
+
+    it('should return the same value before timer is reached', () => {
+        const val1 = 'input1'
+        const val2 = 'input2'
+
+        const {result, rerender } =renderHook(
+            ({ val, delay }) => useDebounce(val, delay),
+            { initialProps: { val: val1, delay: 500 }}
+        )
+
+        expect(result.current).toBe(val1)
+        jest.advanceTimersByTime(490)
+
+        rerender({ val: val2 })
+        expect(result.current).toBe(val1)
+
+        jest.runAllTimers() // fast forward to all timer execution
+        expect(result.current).toBe(val2)
     })
 })
